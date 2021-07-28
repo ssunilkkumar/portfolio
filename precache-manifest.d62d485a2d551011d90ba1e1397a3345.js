2a49588cc1419a8fca1f3253f2339cf2999d2852
Sunil Kumar <ssunil.uk@gmail.com>
2021-07-28 15:52:33 +0530

    Updates

---
+++ precache-manifest.d62d485a2d551011d90ba1e1397a3345.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d14eda51ffa7e81f7af44eae5844005",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "429fdf52d79de6e1be66",
+    "url": "/portfolio/static/css/main.67a0472f.chunk.css"
+  },
+  {
+    "revision": "72293abb4f94521fddb1",
+    "url": "/portfolio/static/js/2.3f18c314.chunk.js"
+  },
+  {
+    "revision": "70b070bc4c4b879d3d3b9683ea8bec9f",
+    "url": "/portfolio/static/js/2.3f18c314.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "429fdf52d79de6e1be66",
+    "url": "/portfolio/static/js/main.4eac11b8.chunk.js"
+  },
+  {
+    "revision": "0647c3197700d00358e9",
+    "url": "/portfolio/static/js/runtime-main.51b06349.js"
+  },
+  {
+    "revision": "21999f09aca6272cd15a16d949434923",
+    "url": "/portfolio/static/media/accenture-3.21999f09.svg"
+  },
+  {
+    "revision": "7516e524df45623611997a89cfe26bf7",
+    "url": "/portfolio/static/media/airtable.7516e524.png"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/portfolio/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "fe1ba489f3ab3a7dca6b24e713243211",
+    "url": "/portfolio/static/media/coschedule.fe1ba489.png"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/portfolio/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/portfolio/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "0555bfb7e81f57a1efe6a53c7490abb7",
+    "url": "/portfolio/static/media/github-api.0555bfb7.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/portfolio/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/portfolio/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/portfolio/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/portfolio/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/portfolio/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "d7c985e7cca077dc2d77f89a66573e35",
+    "url": "/portfolio/static/media/postgresql.d7c985e7.svg"
+  },
+  {
+    "revision": "e1da0271939278b4413962174218411d",
+    "url": "/portfolio/static/media/profile.e1da0271.png"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/portfolio/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/portfolio/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "a5e22e648ab04ed862a71e9ad9456121",
+    "url": "/portfolio/static/media/rentomojo.a5e22e64.png"
+  },
+  {
+    "revision": "c1b5dec15f5f059f6f205f347dcba841",
+    "url": "/portfolio/static/media/sunil.c1b5dec1.jpg"
+  },
+  {
+    "revision": "28e91a033541c2c901bf3fe211945b5e",
+    "url": "/portfolio/static/media/yatra.28e91a03.png"
+  }
+]);
